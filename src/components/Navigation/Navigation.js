fcd5fb1b94ce77d0507ac8734f23370e385ddf20
Ian Hecker <ianhecker@yahoo.com>
2019-03-06 14:37:43 -0700

    Finished user sign in, sign out, and user view based on signed in

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import SignOutButton from '../SignOut/SignOut'
 
 import * as ROUTES from '../../constants/routes'
 
-const Navigation = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
+  <ul>    
     <li>
       <Link to={ROUTES.LANDING}>Landing</Link>
     </li>
@@ -16,9 +18,20 @@ const Navigation = () => (
     </li>
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>    
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
     </li>
     <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
     </li>
   </ul>
 );
