5ec650c7fa7156a658df7cedce23cae164ec8f0c
LAPTOP-Q7OOIVSO\grace <gracewalkuski@gmail.com>
2019-03-09 20:35:51 -0700

    added create member form fields

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-import { withFirebase } from '../Firebase'
-
+import React, { Component } from "react";
+import { withFirebase } from "../Firebase";
 
 class AdminPage extends Component {
   constructor(props) {
@@ -8,24 +7,24 @@ class AdminPage extends Component {
 
     this.state = {
       loading: false,
-      users: [],
+      users: []
     };
   }
 
   componentDidMount() {
     this.setState({ loading: true });
 
-    this.props.firebase.users().on('value', snapshot => {
+    this.props.firebase.users().on("value", snapshot => {
       const usersObject = snapshot.val();
 
       const usersList = Object.keys(usersObject).map(key => ({
         ...usersObject[key],
-        uid: key,
+        uid: key
       }));
-      
+
       this.setState({
         users: usersList,
-        loading: false,
+        loading: false
       });
     });
   }
@@ -33,7 +32,7 @@ class AdminPage extends Component {
   componentWillUnmount() {
     this.props.firebase.users().off();
   }
-  
+
   render() {
     const { users, loading } = this.state;
 
@@ -46,30 +45,71 @@ class AdminPage extends Component {
         <UserList users={users} />
       </div>
     );
-  }  
+  }
 }
 
 const UserList = ({ users }) => (
   <div>
-    <h2>
-      Users
-    </h2>  
+    <h2>Users</h2>
     <ul>
       {users.map(user => (
         <li key={user.uid}>
           <strong>Username:</strong> {user.username}
-          <ul>         
+          <ul>
             <li>
               <strong>ID:</strong> {user.uid}
             </li>
             <li>
               <strong>E-Mail:</strong> {user.email}
-            </li>            
+            </li>
           </ul>
         </li>
       ))}
     </ul>
-  </div>  
+    <h2>Create Member</h2>
+    <form>
+      <input type="text" id="firstname" placeholder="First Name" />
+      <input type="text" id="middlename" placeholder="Middle Name" />
+      <input type="text" id="lastname" placeholder="Last Name" />
+      <br />
+      <input type="text" id="birthday" placeholder="Birthday" />
+      <input type="text" id="phonenumber" placeholder="Phone Number" />
+      <input type="text" id="emailaddress" placeholder="Email" />
+      <br />
+      <input type="text" id="streetaddress" placeholder="Street Address" />
+      <br />
+      <input type="text" id="city" placeholder="City" />
+      <input type="text" id="state" placeholder="State" />
+      <input type="text" id="zip" placeholder="Zip" />
+      <input type="text" id="apartmentnumber" placeholder="Apartment No" />
+      <br />
+      <input type="radio" id="mealsincenter" />
+      Meals In Center
+      <input type="radio" id="mealsonwheels" />
+      Meals On Wheels
+      <h3>Emergency Contact</h3>
+      <input
+        type="text"
+        id="emergencycontactfirstname"
+        placeholder="First Name"
+      />
+      <input
+        type="text"
+        id="emergencycontactlastname"
+        placeholder="Last Name"
+      />
+      <input
+        type="text"
+        id="emergencycontactphonenumber"
+        placeholder="Phone Number"
+      />
+      <input
+        type="text"
+        id="emergencycontactrelationship"
+        placeholder="Relationship"
+      />
+    </form>
+  </div>
 );
 
 export default withFirebase(AdminPage);
