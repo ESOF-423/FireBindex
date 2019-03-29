0dec48fef9c1db97f338f7dd54718c2da967c917
trent <b.trent5@gmail.com>
2019-03-28 20:06:43 -0600

    adding complexComponent shallow render tests

--- src/tests/complexComponents.test.js
+++ src/tests/complexComponents.test.js
@@ -3,12 +3,30 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 
 import Firebase from '../components/Firebase/Firebase.js'
 import PageEvent from '../components/PageEvent/PageEvent.js'
+import CreateEvent from '../components/PageEvent/CreateEvent.js'
+import ViewEvent from '../components/PageEvent/ViewEvent.js'
+
 import PageMember from '../components/PageMember/PageMember.js'
+import CreateMember from '../components/PageMember/CreateMember.js'
+import ViewMember from '../components/PageMember/ViewMember.js'
+
 import PageService from '../components/PageService/PageService.js'
+import CreateService from '../components/PageService/CreateService.js'
+import ViewService from '../components/PageService/ViewService.js'
 
 it('shallow renders PageEvent without exploding', () => {
 	const renderer = new ShallowRenderer();
 	renderer.render(React.createElement(PageEvent));
+});
+
+it('shallow renders CreateEvent without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(CreateEvent));
+});
+
+it('shallow renders ViewEvent without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(ViewEvent));
 });
 
 it('shallow renders PageMember without exploding', () => {
@@ -16,7 +34,27 @@ it('shallow renders PageMember without exploding', () => {
 	renderer.render(React.createElement(PageMember));
 });
 
+it('shallow renders CreateMember without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(CreateMember));
+});
+
+it('shallow renders ViewMember without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(ViewMember));
+});
+
 it('shallow renders PageService without exploding', () => {
 	const renderer = new ShallowRenderer();
 	renderer.render(React.createElement(PageService));
+});
+
+it('shallow renders CreateService without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(CreateService));
+});
+
+it('shallow renders ViewService without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(ViewService));
 });
