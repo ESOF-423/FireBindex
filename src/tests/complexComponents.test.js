789d871231293363cebd410721860fc49a2303ae
trent <b.trent5@gmail.com>
2019-03-28 19:54:55 -0600

    updating yaml and adding complexComponent shells

--- src/tests/complexComponents.test.js
+++ src/tests/complexComponents.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
-import App from '../components/App/App.js'
+import Firebase from '../components/Firebase/Firebase.js'
+import PageEvent from '../components/PageEvent/PageEvent.js'
+import PageMember from '../components/PageMember/PageMember.js'
+import PageService from '../components/PageService/PageService.js'
 
-it('shallow renders App without exploding', () => {
-	// const renderer = new ShallowRenderer();
-	// renderer.render(React.createElement(App));
+it('shallow renders PageEvent without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(PageEvent));
+});
+
+it('shallow renders PageMember without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(PageMember));
+});
+
+it('shallow renders PageService without exploding', () => {
+	const renderer = new ShallowRenderer();
+	renderer.render(React.createElement(PageService));
 });
