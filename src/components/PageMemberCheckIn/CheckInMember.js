f9a4ea0d8c4b6dfe5460fff157b9a09bc0e3ede0
LAPTOP-Q7OOIVSO\grace <gracewalkuski@gmail.com>
2019-04-05 10:47:10 -0600

    styled check in page

--- src/components/PageMemberCheckIn/CheckInMember.js
+++ src/components/PageMemberCheckIn/CheckInMember.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
 import { withFirebase } from "../Firebase";
 
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  dense: {
+    marginTop: 19
+  },
+  menu: {
+    width: 200
+  }
+});
+
 class CheckInMember extends Component {
   constructor(props) {
     super(props);
@@ -25,10 +52,10 @@ class CheckInMember extends Component {
         uid: key
       }));
 
-    //   console.log(membersList)
+      //   console.log(membersList)
 
       for (var i = 0; i < membersList.length; i++) {
-          console.log("in the loooooooop")
+        console.log("in the loooooooop");
         if (
           membersList[i].firstName.toLowerCase() === firstName.toLowerCase() &&
           membersList[i].lastName.toLowerCase() === lastName.toLowerCase() &&
@@ -43,11 +70,11 @@ class CheckInMember extends Component {
 
           console.log(attendance);
           this.props.firebase.attendances().push(attendance);
-          document.getElementById("successMessage").innerHTML = "Sucesss!"
-        }
-        else {
-          document.getElementById("successMessage").innerHTML = "Member not found in database, try again"
-
+          document.getElementById("successMessage").innerHTML = "Sucesss!";
+          break
+        } else {
+          document.getElementById("successMessage").innerHTML =
+            "Member not found in database, try again";
         }
       }
     });
@@ -59,32 +86,65 @@ class CheckInMember extends Component {
 
   render() {
     const { firstName, lastName } = this.state;
+    const { classes } = this.props;
 
     return (
       <div>
-        <h1>Sign in to event</h1>
-
-        <form onSubmit={this.onSubmit}>
-          <input
-            name="firstName"
-            value={firstName}
-            onChange={this.onChange}
-            type="text"
-            placeholder="First Name"
-          />
-          <input
-            name="lastName"
-            value={lastName}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Last Name"
-          />
-          <button type="button" onClick={this.onSubmit}>Check In</button>
-          <div id="successMessage"></div>
-        </form>
+        <Grid container justify="center">
+          <Grid item xs={12} sm={8} md={6} lg={6}>
+            <div align="center" style={{ marginTop: "20px" }}>
+              <Card>
+                <CardHeader
+                  title="Sign in to Event"
+                  subheader="Sign in with your first and last name"
+                />
+                <CardContent>
+                  <form onSubmit={this.onSubmit}>
+                    <TextField
+                      classname={classes.textField}
+                      margin="normal"
+                      name="firstName"
+                      value={firstName}
+                      onChange={this.onChange}
+                      type="text"
+                      label="First Name"
+                      fullWidth
+                      required
+                    />
+                    <TextField
+                      classname={classes.textField}
+                      margin="normal"
+                      name="lastName"
+                      value={lastName}
+                      onChange={this.onChange}
+                      type="text"
+                      label="Last Name"
+                      fullWidth
+                      required
+                    />
+                    <Button
+                      type="button"
+                      onClick={this.onSubmit}
+                      size="large"
+                      color="primary"
+                      variant="contained"
+                    >
+                      Check In
+                    </Button>
+                    <div id="successMessage" />
+                  </form>
+                </CardContent>
+              </Card>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default withFirebase(CheckInMember);
+CheckInMember.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(withFirebase(CheckInMember));
